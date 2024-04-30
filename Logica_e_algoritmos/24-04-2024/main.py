d6081524c565436c33204e6a12210823a436e69e
Luis Eduardo <luiseduardo250305@gmail.com>
2024-04-30 14:45:46 -0300

    Atualizando repositório

---
+++ Logica_e_algoritmos/24-04-2024/main.py
@@ -0,0 +1,13 @@
+"""numero = int(input("Digite um número:"))
+print("Quadrado:", numero * numero)
+print("Metade:", numero / 2 )"""
+
+"""celsius = int(input("Digite a temperatura em celsius:"))
+converter =( (9 * celsius) + 160) / 5
+print("Temperatura em Fahrenheint: ",converter)"""
+
+num1 = int(input("Digite o número 1:"))
+num2 = int(input("Digite o número 2:"))
+num3 = int(input("Digite o número 3:"))
+media = (num1 + num2 + num3) / 3
+print("Media: ", media)
