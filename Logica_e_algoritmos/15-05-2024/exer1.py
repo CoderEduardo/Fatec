84e4fd231af6dfbfd6cd8ccd12e8ae21ef0e54b9
Luis Eduardo <luiseduardo250305@gmail.com>
2024-05-23 17:18:30 -0300

    Atividades feitas

---
+++ Logica_e_algoritmos/15-05-2024/exer1.py
@@ -0,0 +1,4 @@
+preco1 = float(input("Digite o valor do primeiro produto: "))
+preco2 = float(input("Digite o valor do segundo produto: "))
+print(f"Valor do primeiro produtdo com 8% de desconto: { preco1 - (preco1 * 0.08)}")
+print(f"Valor do primeiro produtdo com 11% de desconto: { preco1 - (preco2 * 0.11)}")
