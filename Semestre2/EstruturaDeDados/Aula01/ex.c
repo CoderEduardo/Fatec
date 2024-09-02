c0c1fd5e68f6cf9f8dab7fe31d74cbf25a817772
Luis Eduardo <luiseduardo250305@gmail.com>
2024-09-02 15:58:53 -0300

    Aula 1 de estrutura de dados

---
+++ Semestre2/EstruturaDeDados/Aula01/ex.c
@@ -0,0 +1,11 @@
+int main(int argc, char const *argv[])
+{
+    float mediaA = 8.75;
+    int alunosA = 50;
+    int alunosB = 45;
+    float notaJoao = 9.75;
+
+    printf("A turma A obteve média %f na avaliação de programação estruturada em um total de %i alunos. ", mediaA, alunosA );
+    printf("\nJoão da turma B, em um total de %i , obteve a melhor nota, que foi %f", alunosB ,notaJoao);
+    return 0;
+}
---
+++ Semestre2/EstruturaDeDados/Aula01/main.c
@@ -0,0 +1,11 @@
+int main(int argc, char const *argv[])
+{
+    char sexo;
+    sexo = 'M';
+    int evento = 5;
+    char corrida = 'C';
+    float tempo = 27.5;
+    printf("Seu sexo é: %C \n", sexo);
+    printf("Olá mundo");
+    return 0;
+}
